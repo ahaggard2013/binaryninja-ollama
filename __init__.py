4f3401401990ccb17ca218c3ce64995bcdb360aa
Austin Haggard <10605201+ahaggard2013@users.noreply.github.com>
2024-07-10 21:53:33 -0400

    fixed r string

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 from .plugin import *
 
-PluginCommand.register("Ollama\Rename all functions", "Rename all functions based on (HLIL)", rename_all_functions_command)
+PluginCommand.register(r"Ollama\Rename all functions", "Rename all functions based on (HLIL)", rename_all_functions_command)
 
 PluginCommand.register_for_high_level_il_function(r"Ollama\Rename target function", "Rename target function based on (HLIL)",
                             rename_function_HLIL_command)
@@ -11,7 +11,7 @@ PluginCommand.register_for_high_level_il_function(r"Ollama\Rename all function v
 PluginCommand.register_for_high_level_il_instruction(r"Ollama\Rename target variable", "Rename target variable based on (HLIL)",
                             rename_variable_command)
 
-PluginCommand.register("Ollama\Settings\Set ollama model", "set the model you want to run", set_model_dialog)
+PluginCommand.register(r"Ollama\Settings\Set ollama model", "set the model you want to run", set_model_dialog)
 
-PluginCommand.register("Ollama\Settings\Set ollama server", "set the server where you want to access ollama", set_server_dialog)
+PluginCommand.register(r"Ollama\Settings\Set ollama server", "set the server where you want to access ollama", set_server_dialog)
 
